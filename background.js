852e817d4aebc51031a75a6de0ad55ac6eb4fe2c
Daniel Solawa <danielsolawa@gmail.com>
2017-12-28 23:49:00 +0100

    first commit

---
+++ background.js
@@ -0,0 +1,326 @@
+const CLIENT_ID = '2rciyhzluifcp5gt7dtw2qlm8bvp74';
+const ACCEPT_HEADER = 'application/vnd.twitchtv.v5+json';
+const GET_BY_USERNAME = 'https://api.twitch.tv/kraken/users?login=';
+const GET_BY_USER_ID = 'https://api.twitch.tv/kraken/users/';
+const GET_STREAM = 'https://api.twitch.tv/kraken/streams/';
+const ONE_HOUR_IN_MILLIS  = 1000 * 60 * 62;
+const FIFTEEN_MIN_IN_MILLIS = 1000 * 60 * 15;
+const NOTIFICATION_DISPLAY_TIME = 3000;
+
+
+const TEMP = 1000 * 60;
+
+
+var liveChannels;
+var usersID = [];
+var notificationList = [];
+
+
+
+showAllLiveStreams();
+
+setInterval(showAllLiveStreams, ONE_HOUR_IN_MILLIS);
+setInterval(seekForLiveChannel, FIFTEEN_MIN_IN_MILLIS);
+
+
+
+//shows all channels in the list
+function showAllLiveStreams(){
+	
+	liveChannels = [];
+	fetchStreamList();
+	
+}
+
+
+function seekForLiveChannel(){
+
+
+	fetchStreamList();
+}
+
+
+
+
+
+// Fetches stream list from local storage
+function fetchStreamList(){
+ chrome.storage.local.get({'usersID': []}, function(result){
+
+ 	usersID = result.usersID;
+	fetchLiveStreams();
+ });
+
+}
+
+// fetches live stream data
+function fetchLiveStreams(){
+
+	 var requestCallback = new MyRequestsCompleted({
+        numRequest: usersID.length,
+        singleCallback: function(){
+        notifyAll();
+        
+        setTimeout(clearNotificationList, 20000);
+        }
+    });
+	for(var i = 0; i < usersID.length; i++){
+		$.ajax({
+		type : 'GET',
+		url: GET_STREAM + usersID[i].id,
+		headers : {'Accept' :  ACCEPT_HEADER, 'Client-ID' : CLIENT_ID},
+		success : function(data){requestCallback.requestComplete(data, true)},
+		error : liveError
+	});
+
+	
+	
+
+	}//end of loop
+
+	
+}
+
+
+
+
+
+
+function clearNotificationList(){
+
+
+
+	
+	for(var i = 0; i < notificationList.length; i++){
+		chrome.notifications.clear(notificationList[i].id, function(wasCleared){
+			if(wasCleared){
+				console.log("notifications were removed");
+			}
+			
+	});
+	}
+
+
+	chrome.notifications.getAll(function(notfs){
+		console.log(notfs);
+		console.log("=============================================================");
+	});
+
+
+	notificationList = [];
+
+
+
+
+
+}
+
+
+
+var MyRequestsCompleted = (function() {
+    var numRequestToComplete, 
+        requestsCompleted, 
+        callBacks, 
+        singleCallBack; 
+
+    return function(options) {
+        if (!options) options = {};
+
+        numRequestToComplete = options.numRequest || 0;
+        requestsCompleted = options.requestsCompleted || 0;
+        callBacks = [];
+        var fireCallbacks = function () {
+            for (var i = 0; i < callBacks.length; i++) callBacks[i]();
+        };
+        if (options.singleCallback) callBacks.push(options.singleCallback);
+
+        
+
+        this.addCallbackToQueue = function(isComplete, callback) {
+            if (isComplete) requestsCompleted++;
+            if (callback) callBacks.push(callback);
+            if (requestsCompleted == numRequestToComplete) fireCallbacks();
+        };
+        this.requestComplete = function(data, isComplete) {
+            if (isComplete){
+            	requestsCompleted++;
+            	addToLive(data);
+            } 
+            if (requestsCompleted == numRequestToComplete){
+            	fireCallbacks();
+            } 
+        };
+        this.setCallback = function(callback) {
+            callBacks.push(callBack);
+        };
+    };
+    })();
+
+
+// adds channel to list (if it's live)
+function addToLive(data){
+	
+
+	if(data.stream != null){
+	var channelID = data.stream.channel._id;
+	var channelName = data.stream.channel.name;
+	var channelViewers = data.stream.viewers;
+	var channelStatus = data.stream.channel.status;
+	var channelGame = data.stream.game;
+	var channelUrl = data.stream.channel.url;
+	var channelLogo = data.stream.channel.logo;
+	var channelPreview = data.stream.preview.medium;
+	
+	var liveChannel = {'id' : channelID, 'channelName' : channelName, 
+		'channelViewers' : channelViewers, 'channelStatus': channelStatus, 
+		'channelGame': channelGame, 'channelUrl' : channelUrl, 'channelLogo' : channelLogo,'channelPreview' : channelPreview, 'isNotified' : false};
+		if(!(isChannelAdded(liveChannel.id))){
+			liveChannels.push(liveChannel);
+		}
+		
+			
+		
+		
+}
+
+}
+
+
+function isChannelAdded(id){
+	for(var i = 0; i < liveChannels.length; i++){
+		if(liveChannels[i].id == id){
+			return true;
+		}
+	}
+
+	return false;
+}
+
+
+function notifyAll(){
+	for(var i = 0; i < liveChannels.length; i++){
+		if(!(liveChannels[i].isNotified)){
+			liveChannels[i].isNotified = true;
+			notify(liveChannels[i]);
+		}
+	}
+
+	
+}
+
+
+function notifyList(){
+	var liveList = [];
+	for(var i = 0; i < liveChannels.length; i++){
+		if(!(liveChannels[i].isNotified)){
+			liveChannels[i].isNotified = true;
+			liveList.push({'title': liveChannels[i].channelName,  'message': liveChannels[i].channelViewers + ' viewers\n' + liveChannels[i].channelStatus});
+		}
+	}
+
+	testNot(liveList);
+
+	
+}
+
+
+function testNot(list){
+var notification = {
+		type : "list",
+		title: "Live Steams",
+		iconUrl: "../icon.png",
+		message: "message",
+		items: list
+	};
+
+	chrome.notifications.create(notification, function(notificationId){
+		streamId = notificationId;
+	});
+
+
+}
+
+
+
+
+//notifys user about live streams
+function notify(stream){
+	var notification = {
+		type : "image",
+		title: stream.channelName + " is live" + " " + stream.channelViewers + " viewers",
+		iconUrl: stream.channelLogo,
+		imageUrl: stream.channelPreview,
+		message: stream.channelStatus + "\nGame: " + stream.channelGame,
+		eventTime: getDisplayTime(),
+		isClickable : true
+	};
+
+	var url = stream.channelUrl;
+	var streamId = "";
+
+	console.log(getCurrentTime() + " name: " +stream.channelName + " notified");
+
+
+	chrome.notifications.create(notification, function(notificationId){
+		streamId = notificationId;
+		notificationList.push({'id' : streamId});
+	});
+
+
+
+	
+
+	chrome.notifications.onClicked.addListener(function(notificationId){
+		if(notificationId == streamId){
+			window.open(url, "_blank");
+
+
+			chrome.notifications.clear(streamId, function(wasCleared){
+			
+		});
+		}
+		
+	});
+
+
+
+}
+
+
+function getCurrentTime(){
+	var d = new Date();
+	var h = d.getHours();
+	var m = d.getMinutes();
+	var s = d.getSeconds();
+
+
+	var currentTime = h +":" + m + ":" + s;
+
+	return currentTime;
+
+
+}
+
+
+function getDisplayTime(){
+	var date = Date.now();
+
+	return date + NOTIFICATION_DISPLAY_TIME;
+}
+
+
+
+
+
+
+function liveError(data){
+	console.log("something went wrong");
+}
+
+
+
+
+
+
+
+
---
+++ extension.js
@@ -0,0 +1,298 @@
+const CLIENT_ID = '2rciyhzluifcp5gt7dtw2qlm8bvp74';
+const ACCEPT_HEADER = 'application/vnd.twitchtv.v5+json';
+const GET_BY_USERNAME = 'https://api.twitch.tv/kraken/users?login=';
+const GET_BY_USER_ID = 'https://api.twitch.tv/kraken/users/';
+const GET_STREAM = 'https://api.twitch.tv/kraken/streams/';
+const ONE_HOUR_IN_MILLIS  = 1000 * 60  * 60;
+
+var streamPanelHidden = true;
+var list = [];
+var channelList = [];
+
+$("#showLive").click(function(){
+	checkForLive();
+    $("#main").toggle();
+});
+
+$("#manageStreams").click(function(){
+	showList();
+    $("#manage").toggle();
+});
+
+$("#addChannel").click(function(){
+	addStreamToList();
+});
+
+
+
+
+
+function showList(){
+	
+	
+	chrome.storage.local.get({"usersID" : []}, function(result){
+		usersID = result.usersID;
+		createChannelList(usersID);
+	});
+
+	
+
+	
+}
+
+
+function createChannelList(usersID){
+
+
+	$("#manage").html("");
+
+	for(var i = 0; i < usersID.length; i++){
+			var us = usersID[i].id;
+
+			var listDiv = document.createElement("div");
+			listDiv.setAttribute("class", us);
+			
+
+			var streamID = document.createElement("span");
+			streamID.appendChild(document.createTextNode(usersID[i].username));
+
+			var remove = document.createElement("button");
+
+			remove.setAttribute("id", us);
+			remove.setAttribute("type", "button");
+			remove.setAttribute("class", "removeButton");
+			remove.appendChild(document.createTextNode("remove"));
+			remove.addEventListener("click", removeFromList, false);
+			
+
+
+			listDiv.appendChild(streamID);
+			listDiv.appendChild(remove);
+
+			
+
+		$("#manage").append(listDiv);
+	}
+
+
+}
+
+
+function removeFromList(e){
+	var index = getIndex(this.id);
+	if(index > -1){
+		var streamToRemove = $("." + this.id);
+		removeFromStorage(index);
+		streamToRemove.remove();
+	}
+	
+}
+
+
+function removeFromStorage(index){
+	usersID.splice(index, 1);
+
+	chrome.storage.local.set({'usersID' : usersID}, function(){
+				 $("#manage").toggle();
+				showMessage(true, "Channel removed sucessfully");
+
+			});
+
+}
+
+
+function getIndex(id){
+	for(var i = 0; i < usersID.length; i++){
+		if(usersID[i].id == id){
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+
+
+// Checks is any channel live
+function checkForLive(){
+ chrome.storage.local.get({'usersID': [] }, function(result){
+ 	usersID = result.usersID;
+
+ 	fetchStreams();
+
+
+ });
+
+}
+
+function fetchStreams(){
+
+	$("#main").html("");
+	for(var i = 0; i < usersID.length; i++){
+		$.ajax({
+		type : 'GET',
+		url: GET_STREAM + usersID[i].id,
+		headers : {'Accept' :  ACCEPT_HEADER, 'Client-ID' : CLIENT_ID},
+		success : addToList,
+		error : liveError
+	});
+	}
+}
+
+
+function addToList(data){
+
+	if(data.stream != null){
+
+		var list = document.createElement("div");
+		list.setAttribute("class", "list");
+
+		var stream = document.createElement("div");
+
+
+		
+		var streamLink = document.createElement("a");
+		
+		streamLink.setAttribute("href", data.stream.channel.url);
+		streamLink.setAttribute("target", '_blank');
+		streamLink.appendChild(document.createTextNode(data.stream.channel.display_name 
+			+ " - " + data.stream.game));
+
+		
+
+
+		stream.appendChild(streamLink);
+		list.appendChild(stream);
+		
+		$("#main").append(list);
+
+
+		}
+
+		
+	
+}
+
+
+
+
+
+function liveError(){
+	console.log("something went wrong");
+}
+
+
+//adds channel to local storage
+
+function addStreamToList(){
+	var streamName = document.getElementById("streamName").value;
+
+	if(streamName.length > 0){
+			$.ajax({
+		type : 'GET',
+		url: GET_BY_USERNAME + streamName,
+		headers : {'Accept' :  ACCEPT_HEADER, 'Client-ID' : CLIENT_ID},
+		success : checkChannel,
+		error : saveError
+	});
+	}else{
+		showMessage(false, "This field can't be empty");
+	}
+
+
+}
+
+
+
+function checkChannel(data){
+	$("#streamName").val("");
+	
+	if(data.users.length == 0){
+		wrongName();
+	}else{
+		saveChannel(data);
+	}
+	
+	
+}
+
+// saves channel id 
+function saveChannel(data){
+	var userID = data.users[0]._id;
+	var userName = data.users[0].display_name;
+
+	chrome.storage.local.get({'usersID' : [] }, function(result){
+		var usersID = result.usersID;
+
+		if(!isAlreadyStored(userID, usersID)){
+			usersID.push({'id' : userID, 'username' : userName});
+			chrome.storage.local.set({'usersID' : usersID}, function(){
+				showMessage(true, "Channel added sucessfully");
+
+			});
+			
+		}else{
+			showMessage(false, "Channel is already saved");
+		}
+	
+	});
+
+	
+
+}
+
+
+function showMessage(isSuccess, message){
+	var text = document.createElement("div");
+	
+	if(isSuccess){
+		text.setAttribute("class", "success");
+	}else{
+		text.setAttribute("class", "error");
+	}
+
+	text.appendChild(document.createTextNode(message));
+
+	$("#message").append(text);
+
+
+	setTimeout(clearMessageTab, 3000);
+}
+
+function wrongName(){
+	var error = document.createElement("div");
+	error.setAttribute("class", "error");
+	error.appendChild(document.createTextNode("Wrong channel name"));
+	
+	$("#message").append(error);
+
+	setTimeout(clearMessageTab, 3000);
+}
+
+
+function clearMessageTab(){
+	$("#message").html("");
+}
+
+function isAlreadyStored(userID, usersID){
+
+	for(var i = 0; i < usersID.length; i++){
+		if(usersID[i].id == userID){
+			return true;
+		}
+	}
+
+	return false;
+
+}
+
+
+function saveError(){
+	alert("error");
+}
+
+
+
+
+
+
